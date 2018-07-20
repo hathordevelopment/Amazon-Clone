af7906b12f9cccb6ccadb0c036d185b46295be2f
Alexis Goupillot <hathor.development@gmail.com>
2018-07-20 08:49:50 +0200

    Added Angular dependencies | Added dependencies to AppModule | Setting up Navbar and footer

---
+++ client/AngularAmazonClone/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app';
+  searchTerm = '';
+  isCollapsed = true;
+
+  getToken () {
+    return localStorage.getItem('token');
+  }
+
+  collapse () {
+    this.isCollapsed = true;
+  }
+
+  closeDropdown(dropdown) {
+    dropdown.close();
+  }
+
+  logout() {}
+
+  search() {}
+}
