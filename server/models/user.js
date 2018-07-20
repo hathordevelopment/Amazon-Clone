34d77370246694ea56bea36fc8fd6ca7c9043cf6
Alexis Goupillot <hathor.development@gmail.com>
2018-07-20 14:20:25 +0200

    Encrypted User password using BCrypt-nodejs

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const UserSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
@@ -16,4 +17,17 @@ const UserSchema = new Schema({
         postalCode: String
     },
     created: { type: Date, default: Date.now }
+});
+
+UserSchema.pre('save', (next) => {
+    var user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, null, null, (err, hash) => {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+    });
 });
